105edea36790894b400c462d1eb2d60153059d72
hagyun93 <imkimhagyun@naver.com>
2023-03-26 20:38:14 +0900

    Handler Interceptor

--- src/main/java/me/whiteship/WebConfig.java
+++ src/main/java/me/whiteship/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -15,6 +16,23 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    // preHandle 1
+    // preHandle 2
+    // 요청 처리
+    // postHandler 2
+    // postHandler 1
+    // 뷰 랜더링
+    // afterCompletion 2
+    // afterCompletion 1
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // order 설정 안하면 add 한 순서대로
+        registry.addInterceptor(new GreetingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor())
+                .addPathPatterns("/hello")
+                .order(1);
+    }
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
